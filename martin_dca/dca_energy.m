7b6986677874f9466cc35ce280b26463c1fef7b3
kalyaniasthana <kalyaniasthana@gmail.com>
2020-07-18 06:50:05 +0530

    :pencil: test_README is the real README now

--- martin_dca/dca_energy.m
+++
@@ -1,8 +0,0 @@
-try
-	fasta_test = '/media/Data/consensus/temp_files/only_hmm_emitted.fasta';
-	fasta_train = '/media/Data/consensus/temp_files/train_file.fasta';
-	accession = 'PF00902';
-	[score_train, score_test] = calculate_dca_scores(fasta_train,fasta_test,accession)
-catch
-	exit
-end
