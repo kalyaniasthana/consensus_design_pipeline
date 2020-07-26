bee589ab51affd87d80496e22eb5e7d91a40d29d
kalyaniasthana <kalyaniasthana@gmail.com>
2020-07-26 15:56:23 +0530

    DCA energy for both consensuses

--- martin_dca/calculate_dca_scores.m
+++ martin_dca/calculate_dca_scores.m
@@ -9,17 +9,15 @@ function [score_train, score_test] = calculate_dca_scores(fasta_train,fasta_test
 score_train = score_fct( fasta_train , eij, hi, N, q);
 score_test = score_fct( fasta_test , eij, hi, N, q);
 score_train_file = strcat(home,'/scores_refined_alignment/', accession, '_scores_refined.txt');
-score_test_file = strcat(home, '/scores_hmm_sequences/', accession, '_scores_hmm.txt');
-% dlmwrite('/media/Data/consensus_project/temp_files/test_data.txt',score_train,'delimiter',' ');  
+score_test_file = strcat(home, '/scores_hmm_sequences/', accession, '_scores_hmm.txt');  
 dlmwrite(score_train_file,score_train,'delimiter',' ');
 dlmwrite(score_test_file,score_test,'delimiter',' ');
 
-% cf = strcat('/media/Data/consensus/all_consensus_sequences/', accession);
-% consensus_file = strcat(cf, '_consensus.fasta');
-% score_consensus = score_fct( consensus_file, eij, hi, N, q);
+consensus_train_file=strcat(home,'/refined_consensuses/',accession,'_refined_consensus.fasta');
+score_consensus_train = score_fct(consensus_train_file, eij, hi, N, q);
 
-%fprintf('%d\n', score_consensus(1));
-%fprintf('%d\n', score_consensus(2));
+consensus_test_file=strcat(home,'/hmm_consensuses/',accession,'_hmm_consensus.fasta');
+score_consensus_test = score_fct(consensus_test_file,eij,hi,N,q);
 
 figure;
 histogram(-score_train, 'Normalization', 'prob', 'BinWidth', 20);
@@ -32,25 +30,24 @@ file = fopen(fname, 'w');
 histogram(-score_test, 'Normalization', 'prob', 'BinWidth', 20);
 xlimtest = get(gca, 'xlim');
 hold;
-% y1 = get(gca, 'ylim');
-% line([-score_consensus(1) -score_consensus(1)], y1, 'Color', 'g');
-% hold;
-% line([-score_consensus(2) -score_consensus(2)], y1, 'Color', 'r');
-s = strcat('DCA energy for: ', accession);
-xlabel(s);
 
-legend('Refined MSA','HMM emitted sequences');
-% legend('Refined MSA', 'HMM emitted MSA', 'Consensus from Refined MSA', 'Consensus from hmm emitted MSA');
-% plt = strcat('../dca_energy_plots/', accession);
+y1 = get(gca, 'ylim');
+
+line([-score_consensus_train -score_consensus_train], y1, 'Color', 'g');
+hold;
+line([-score_consensus_test -score_consensus_test], y1, 'Color', 'r');
+
+label_x = strcat('DCA energies for: ', accession);
+xlabel(label_x);
+
+legend('Refined MSA', 'HMM emitted MSA', 'Consensus from Refined MSA', 'Consensus from hmm emitted MSA');
 plot_name = strcat(home, '/dca_energy_plots/', accession, '_dca_energies');
-%ylim([0 0.04])
 print(plot_name, '-dpng');
 
 % train stats
 mean_train = -mean(score_train);
 mode_train = -mode(score_train);
 sd_train = std(score_train);
-% consensus_train = -score_consensus(1);
 min_x_train = -max(score_train);
 max_x_train = -min(score_train);
 
@@ -58,7 +55,7 @@ fprintf(file, '%s\n', 'TRAIN STATS (Refined Alignment)');
 fprintf(file, '%s\t%.6f\n', 'Mean: ', mean_train);
 fprintf(file, '%s\t%.6f\n', 'Mode: ', mode_train);
 fprintf(file, '%s\t%.6f\n', 'SD: ', sd_train);
-% fprintf(file, '%s\t%.6f\n', 'Consensus energy: ', consensus_train);
+fprintf(file, '%s\t%.6f\n', 'Consensus energy: ',score_consensus_train);
 fprintf(file, '%s\t%.6f\n', 'Min x: ', min_x_train);
 fprintf(file, '%s\t%.6f\n', 'Max x: ', max_x_train);
 
@@ -66,7 +63,6 @@ fprintf(file, '%s\t%.6f\n', 'Max x: ', max_x_train);
 mean_test = -mean(score_test);
 mode_test = -mode(score_test);
 sd_test = std(score_test);
-% consensus_test = -score_consensus(2);
 min_x_test = -max(score_test);
 max_x_test = -min(score_test);
 
@@ -75,7 +71,7 @@ fprintf(file, '%s\n', 'TEST STATS (HMM emitted sequences)');
 fprintf(file, '%s\t%.6f\n', 'Mean: ', mean_test);
 fprintf(file, '%s\t%.6f\n', 'Mode: ', mode_test);
 fprintf(file, '%s\t%.6f\n', 'SD: ', sd_test);
-% fprintf(file, '%s\t%.6f\n', 'Consensus energy: ', consensus_test);
+fprintf(file, '%s\t%.6f\n', 'Consensus energy: ', score_consensus_test);
 fprintf(file, '%s\t%.6f\n', 'Min x: ', min_x_test);
 fprintf(file, '%s\t%.6f\n', 'Max x: ', max_x_test);
 
